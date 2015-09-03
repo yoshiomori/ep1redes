be539021d840852d4e70202fa03ba064f1a5cc2d
Yoshio Mori <yoshio.mori@hotmail.com>
2015-09-03 02:38:45 -0300

    Implementação
    JOIN

--- ep1irc.c
+++ ep1irc.c
@@ -46,207 +46,242 @@
 #define MAXLINE 4096
 
 int main (int argc, char **argv) {
-   /* Os sockets. Um que será o socket que vai escutar pelas conexões
-    * e o outro que vai ser o socket específico de cada conexão */
-	int listenfd, connfd;
-   /* Informações sobre o socket (endereço e porta) ficam nesta struct */
-	struct sockaddr_in servaddr;
-   /* Retorno da função fork para saber quem é o processo filho e quem
-    * é o processo pai */
-   pid_t childpid;
-   /* Armazena linhas recebidas do cliente */
-   char	recvline[MAXLINE + 1], command[MAXLINE + 1], params[MAXLINE + 1];
-   /* Armazena o tamanho da string lida do cliente */
-   ssize_t n;
-   /* Iniciando o gerenciador de clientes */
-   Client *client = NULL;
-   char nickname[10] = "", host[100] = "", username[100] = "";
-   /* Armazena o índice do vetor de clientes */
-   int i;
-   int quit = 1;
-   Channel *clients = init_client_manager();
+  /* Os sockets. Um que será o socket que vai escutar pelas conexões
+   * e o outro que vai ser o socket específico de cada conexão */
+  int listenfd, connfd;
+  /* Informações sobre o socket (endereço e porta) ficam nesta struct */
+  struct sockaddr_in servaddr;
+  /* Retorno da função fork para saber quem é o processo filho e quem
+   * é o processo pai */
+  pid_t childpid;
+  /* Armazena linhas recebidas do cliente */
+  char	recvline[MAXLINE + 1], command[MAXLINE + 1], param1[MAXLINE + 1], param2[MAXLINE + 1];
+  /* Armazena o tamanho da string lida do cliente */
+  ssize_t n;
+  /* Iniciando o gerenciador de clientes */
+  Client *client = NULL;
+  char nickname[10] = "", host[100] = "", username[100] = "";
+  /* Armazena o índice do vetor de clientes */
+  int i;
+  int quit = 1;
+  Channel *clients = init_client_manager();
+  Channel *sbt = init_client_manager();
+  Channel *globo = init_client_manager();
+  strcpy(sbt->name,"sbt");
+  strcpy(globo->name,"globo");
    
-	if (argc != 2) {
-      fprintf(stderr,"Uso: %s <Porta>\n",argv[0]);
-      fprintf(stderr,"Vai rodar um servidor de echo na porta <Porta> TCP\n");
-		exit(1);
-	}
+  if (argc != 2) {
+    fprintf(stderr,"Uso: %s <Porta>\n",argv[0]);
+    fprintf(stderr,"Vai rodar um servidor de echo na porta <Porta> TCP\n");
+    exit(1);
+  }
 
-   /* Criação de um socket. Eh como se fosse um descritor de arquivo. Eh
-    * possivel fazer operacoes como read, write e close. Neste
-    * caso o socket criado eh um socket IPv4 (por causa do AF_INET),
-    * que vai usar TCP (por causa do SOCK_STREAM), já que o IRC
-    * funciona sobre TCP, e será usado para uma aplicação convencional sobre
-    * a Internet (por causa do número 0) */
-	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket :(\n");
-		exit(2);
-	}
+  /* Criação de um socket. Eh como se fosse um descritor de arquivo. Eh
+   * possivel fazer operacoes como read, write e close. Neste
+   * caso o socket criado eh um socket IPv4 (por causa do AF_INET),
+   * que vai usar TCP (por causa do SOCK_STREAM), já que o IRC
+   * funciona sobre TCP, e será usado para uma aplicação convencional sobre
+   * a Internet (por causa do número 0) */
+  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    perror("socket :(\n");
+    exit(2);
+  }
 
-   /* Agora é necessário informar os endereços associados a este
-    * socket. É necessário informar o endereço / interface e a porta,
-    * pois mais adiante o socket ficará esperando conexões nesta porta
-    * e neste(s) endereços. Para isso é necessário preencher a struct
-    * servaddr. É necessário colocar lá o tipo de socket (No nosso
-    * caso AF_INET porque é IPv4), em qual endereço / interface serão
-    * esperadas conexões (Neste caso em qualquer uma -- INADDR_ANY) e
-    * qual a porta. Neste caso será a porta que foi passada como
-    * argumento no shell (atoi(argv[1]))
-    */
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family      = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(atoi(argv[1]));
-	if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
-		perror("bind :(\n");
-		exit(3);
-	}
+  /* Agora é necessário informar os endereços associados a este
+   * socket. É necessário informar o endereço / interface e a porta,
+   * pois mais adiante o socket ficará esperando conexões nesta porta
+   * e neste(s) endereços. Para isso é necessário preencher a struct
+   * servaddr. É necessário colocar lá o tipo de socket (No nosso
+   * caso AF_INET porque é IPv4), em qual endereço / interface serão
+   * esperadas conexões (Neste caso em qualquer uma -- INADDR_ANY) e
+   * qual a porta. Neste caso será a porta que foi passada como
+   * argumento no shell (atoi(argv[1]))
+   */
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family      = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port        = htons(atoi(argv[1]));
+  if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+    perror("bind :(\n");
+    exit(3);
+  }
 
-   /* Como este código é o código de um servidor, o socket será um
-    * socket passivo. Para isto é necessário chamar a função listen
-    * que define que este é um socket de servidor que ficará esperando
-    * por conexões nos endereços definidos na função bind. */
-	if (listen(listenfd, LISTENQ) == -1) {
-		perror("listen :(\n");
-		exit(4);
-	}
+  /* Como este código é o código de um servidor, o socket será um
+   * socket passivo. Para isto é necessário chamar a função listen
+   * que define que este é um socket de servidor que ficará esperando
+   * por conexões nos endereços definidos na função bind. */
+  if (listen(listenfd, LISTENQ) == -1) {
+    perror("listen :(\n");
+    exit(4);
+  }
 
-   printf("[Servidor no ar. Aguardando conexoes na porta %s]\n",argv[1]);
-   printf("[Para finalizar, pressione CTRL+c ou rode um kill ou killall]\n");
+  printf("[Servidor no ar. Aguardando conexoes na porta %s]\n",argv[1]);
+  printf("[Para finalizar, pressione CTRL+c ou rode um kill ou killall]\n");
    
-   /* O servidor no final das contas é um loop infinito de espera por
-    * conexões e processamento de cada uma individualmente */
-	for (;;) {
-      /* O socket inicial que foi criado é o socket que vai aguardar
-       * pela conexão na porta especificada. Mas pode ser que existam
-       * diversos clientes conectando no servidor. Por isso deve-se
-       * utilizar a função accept. Esta função vai retirar uma conexão
-       * da fila de conexões que foram aceitas no socket listenfd e
-       * vai criar um socket específico para esta conexão. O descritor
-       * deste novo socket é o retorno da função accept. */
-		if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
-			perror("accept :(\n");
-			exit(5);
-		}
+  /* O servidor no final das contas é um loop infinito de espera por
+   * conexões e processamento de cada uma individualmente */
+  for (;;) {
+    /* O socket inicial que foi criado é o socket que vai aguardar
+     * pela conexão na porta especificada. Mas pode ser que existam
+     * diversos clientes conectando no servidor. Por isso deve-se
+     * utilizar a função accept. Esta função vai retirar uma conexão
+     * da fila de conexões que foram aceitas no socket listenfd e
+     * vai criar um socket específico para esta conexão. O descritor
+     * deste novo socket é o retorno da função accept. */
+    if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
+      perror("accept :(\n");
+      exit(5);
+    }
       
-      /* Agora o servidor precisa tratar este cliente de forma
-       * separada. Para isto é criado um processo filho usando a
-       * função fork. O processo vai ser uma cópia deste. Depois da
-       * função fork, os dois processos (pai e filho) estarão no mesmo
-       * ponto do código, mas cada um terá um PID diferente. Assim é
-       * possível diferenciar o que cada processo terá que fazer. O
-       * filho tem que processar a requisição do cliente. O pai tem
-       * que voltar no loop para continuar aceitando novas conexões */
-      /* Se o retorno da função fork for zero, é porque está no
-       * processo filho. */
-      if ( (childpid = fork()) == 0) {
-         /**** PROCESSO FILHO ****/
-         printf("[Uma conexao aberta]\n");
-         /* Já que está no processo filho, não precisa mais do socket
-          * listenfd. Só o processo pai precisa deste socket. */
-         close(listenfd);
+    /* Agora o servidor precisa tratar este cliente de forma
+     * separada. Para isto é criado um processo filho usando a
+     * função fork. O processo vai ser uma cópia deste. Depois da
+     * função fork, os dois processos (pai e filho) estarão no mesmo
+     * ponto do código, mas cada um terá um PID diferente. Assim é
+     * possível diferenciar o que cada processo terá que fazer. O
+     * filho tem que processar a requisição do cliente. O pai tem
+     * que voltar no loop para continuar aceitando novas conexões */
+    /* Se o retorno da função fork for zero, é porque está no
+     * processo filho. */
+    if ( (childpid = fork()) == 0) {
+      /**** PROCESSO FILHO ****/
+      printf("[Uma conexao aberta]\n");
+      /* Já que está no processo filho, não precisa mais do socket
+       * listenfd. Só o processo pai precisa deste socket. */
+      close(listenfd);
          
-         /* Agora pode ler do socket e escrever no socket. Isto tem
-          * que ser feito em sincronia com o cliente. Não faz sentido
-          * ler sem ter o que ler. Ou seja, neste caso está sendo
-          * considerado que o cliente vai enviar algo para o servidor.
-          * O servidor vai processar o que tiver sido enviado e vai
-          * enviar uma resposta para o cliente (Que precisará estar
-          * esperando por esta resposta) 
-          */
+      /* Agora pode ler do socket e escrever no socket. Isto tem
+       * que ser feito em sincronia com o cliente. Não faz sentido
+       * ler sem ter o que ler. Ou seja, neste caso está sendo
+       * considerado que o cliente vai enviar algo para o servidor.
+       * O servidor vai processar o que tiver sido enviado e vai
+       * enviar uma resposta para o cliente (Que precisará estar
+       * esperando por esta resposta) 
+       */
 
-         /* ========================================================= */
-         /* ========================================================= */
-         /*                         EP1 INÍCIO                        */
-         /* ========================================================= */
-         /* ========================================================= */
-         /* TODO: É esta parte do código que terá que ser modificada
-          * para que este servidor consiga interpretar comandos IRC   */
-         while (quit) {
-	   n=read(connfd, recvline, MAXLINE);
-	   if(n < 0)
-	     break;
-            recvline[n]=0;
-            printf("[Cliente conectado no processo filho %d enviou:] ",getpid());
-            if ((fputs(recvline,stdout)) == EOF) {
-               perror("fputs :( \n");
-               exit(6);
-            }
-	    /* Quebrando a mensagem em tokens */
-	    sscanf(recvline, "%s %s", command, params);
-	    recvline[0] = '\0';
-	    /* Interpretando os tokens */
-	    if(!strcmp("NICK", command)){
-	      /* Tratamento do comando NICK */
-	      /* TODO: Pegar informações do cliente */
-	      /* TODO: Protocolo de entrada do controle de concorrencia */
-	      if(!strlen(nickname)){
-		/* Novo cliente */
-		/* Tratando colisão */
-		if(search_client(clients, params) != (Client*)-1)
-		  sprintf(recvline, "%s :Nickname collision KILL\n", params);
-		else{
-		  strcpy(nickname, params);
-		  /* Inserindo novo cliente */
-		  /* TODO: Atualizar a inserção do host do cliente */
-		  insert_client(clients, nickname, "host");
-		}
-	      }
-	      else{
-		/* Cliente atualizando nickname */
-		client = search_client(clients, nickname);
-		/* Tratando colisão */
-		if(search_client(clients, params) != (Client*)-1)
-		  sprintf(recvline, "%s :Nickname is already in use\n", params);
-		else{
-		  strcpy(nickname, params);
-		  strcpy(client->nickname, nickname);
-		}
-	      }
-	      /* TODO: Protocolo de saída do controle de concorrencia */
-	    } else if(!strcmp("LIST", command) && strlen(nickname)){
-	      /* Lista os canais */
-	    } else if(!strcmp("JOIN", command) && strlen(nickname)){
-	      /* Entra no canal */
-	    } else if(!strcmp("PRIVMSG", command) && strlen(nickname)){
-	      /* Enviar mensagem para o canal especificado */
-	    } else if(!strcmp("DCC", command) && strlen(nickname)){
-	      /* Enviar uma arquivo para o cliente especificado */
-	    } else if(!strcmp("PART", command) && strlen(nickname)){
-	      /* Saír do canal */
-	    } else if(!strcmp("QUIT", command)){
-	      /* Desconectar do servidor */
-	      quit = 0;
-	      client = search_client(clients, nickname);
-	      remove_client(clients, client);
-	    } else{
-	      /* Comando não válido */
-	      if(strlen(nickname)){
-		/* Usuário registrado mas comando inexistente */
-		sprintf(recvline, "%s :Unknown command\n", command);
-	      } else
-		sprintf(recvline, ":You have not registered\n", params);
-	    }
-	    /* Se tiver mensagem envia */
-	    if(recvline)
-	      write(connfd, recvline, strlen(recvline));
-         }
-         /* ========================================================= */
-         /* ========================================================= */
-         /*                         EP1 FIM                           */
-         /* ========================================================= */
-         /* ========================================================= */
-
-         /* Após ter feito toda a troca de informação com o cliente,
-          * pode finalizar o processo filho */
-         printf("[Uma conexao fechada]\n");
-         exit(0);
-      }
-      /**** PROCESSO PAI ****/
-      /* Se for o pai, a única coisa a ser feita é fechar o socket
-       * connfd (ele é o socket do cliente específico que será tratado
-       * pelo processo filho) */
-		close(connfd);
+      /* ========================================================= */
+      /* ========================================================= */
+      /*                         EP1 INÍCIO                        */
+      /* ========================================================= */
+      /* ========================================================= */
+      /* TODO: É esta parte do código que terá que ser modificada
+       * para que este servidor consiga interpretar comandos IRC   */
+      while (quit) {
+	n=read(connfd, recvline, MAXLINE);
+	if(n < 0)
+	  break;
+	recvline[n]=0;
+	printf("[Cliente conectado no processo filho %d enviou:] ",getpid());
+	if ((fputs(recvline,stdout)) == EOF) {
+	  perror("fputs :( \n");
+	  exit(6);
 	}
-	exit(0);
+	/* Quebrando a mensagem em tokens */
+	sscanf(recvline, "%s %s", command, param1);
+	recvline[0] = '\0';
+	/* Interpretando os tokens */
+	if(!strcmp("NICK", command)){
+	  /* Tratamento do comando NICK */
+	  /* TODO: Pegar informações do cliente */
+	  /* TODO: Protocolo de entrada do controle de concorrencia */
+	  if(!strlen(nickname)){
+	    /* Novo cliente */
+	    /* Tratando colisão */
+	    if(search_client(clients, param1) != (Client*)-1)
+	      sprintf(recvline, "%s :Nickname collision KILL\n", param1);
+	    else{
+	      strcpy(nickname, param1);
+	      /* Inserindo novo cliente */
+	      /* TODO: Atualizar a inserção do host do cliente */
+	      insert_client(clients, nickname, "host");
+	    }
+	  }
+	  else{
+	    /* Cliente atualizando nickname */
+	    client = search_client(clients, nickname);
+	    /* Tratando colisão */
+	    if(search_client(clients, param1) != (Client*)-1)
+	      sprintf(recvline, "%s :Nickname is already in use\n", param1);
+	    else{
+	      strcpy(nickname, param1);
+	      strcpy(client->nickname, nickname);
+	    }
+	  }
+	  /* TODO: Protocolo de saída do controle de concorrencia */
+	} else if(!strcmp("LIST", command) && strlen(nickname)){
+	  /* Lista os canais */
+	  sprintf(recvline, ":%s\n%s\n", sbt->name, globo->name);
+	} else if(!strcmp("JOIN", command) && strlen(nickname)){
+	  /* Entra no canal */
+	  if(strlen(param1)){
+	    if(!strcmp(sbt->name, param1)){
+	      if(search_client(sbt, nickname) == (Client*)-1)
+		if(insert_client(sbt, nickname, "host"))
+		  sprintf(recvline, "%s :Cannot join channel\n", sbt->name);
+	    }
+	    else if(!strcmp(globo->name, param1)){
+	      if(search_client(globo, nickname) == (Client*)-1)
+		if(insert_client(globo, nickname, "host"))
+		  sprintf(recvline, "%s :Cannot join channel\n", globo->name);
+	    }
+	    else
+	      sprintf(recvline, "%s :No such channel\n", param1);
+	    if(strlen(param2)){
+	      if(!strcmp(sbt->name, param2)){
+		if(search_client(sbt, nickname) == (Client*)-1)
+		  if(insert_client(sbt, nickname, "host"))
+		    sprintf(recvline, "%s :Cannot join channel\n", sbt->name);
+	      }
+	      else if(!strcmp(globo->name, param2)){
+		if(search_client(globo, nickname) == (Client*)-1)
+		  if(insert_client(globo, nickname, "host"))
+		    sprintf(recvline, "%s :Cannot join channel\n", globo->name);
+	      }
+	      else
+		sprintf(recvline, "%s :No such channel\n", param2);
+	    }
+	  }
+	  else
+	    sprintf(recvline, "%s :Not enough parameters\n", command);
+	} else if(!strcmp("PRIVMSG", command) && strlen(nickname)){
+	  /* Enviar mensagem para o canal especificado */
+	} else if(!strcmp("DCC", command) && strlen(nickname)){
+	  /* Enviar uma arquivo para o cliente especificado */
+	} else if(!strcmp("PART", command) && strlen(nickname)){
+	  /* Saír do canal */
+	} else if(!strcmp("QUIT", command)){
+	  /* Desconectar do servidor */
+	  quit = 0;
+	  client = search_client(clients, nickname);
+	  remove_client(clients, client);
+	} else{
+	  /* Comando não válido */
+	  if(strlen(nickname)){
+	    /* Usuário registrado mas comando inexistente */
+	    sprintf(recvline, "%s :Unknown command\n", command);
+	  } else
+	    sprintf(recvline, ":You have not registered\n", param1);
+	}
+	/* Se tiver mensagem envia */
+	if(recvline)
+	  write(connfd, recvline, strlen(recvline));
+      }
+      /* ========================================================= */
+      /* ========================================================= */
+      /*                         EP1 FIM                           */
+      /* ========================================================= */
+      /* ========================================================= */
+
+      /* Após ter feito toda a troca de informação com o cliente,
+       * pode finalizar o processo filho */
+      printf("[Uma conexao fechada]\n");
+      exit(0);
+    }
+    /**** PROCESSO PAI ****/
+    /* Se for o pai, a única coisa a ser feita é fechar o socket
+     * connfd (ele é o socket do cliente específico que será tratado
+     * pelo processo filho) */
+    close(connfd);
+  }
+  exit(0);
 }
